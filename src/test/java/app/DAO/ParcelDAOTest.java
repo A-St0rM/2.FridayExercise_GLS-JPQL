55fcb21ed5bf1fa6dd306f9841b849fd175b1278
Alissa Andrea Storm <140435292+A-St0rM@users.noreply.github.com>
2025-08-29 11:06:00 +0200

    In progress of test

---
+++ src/test/java/app/DAO/ParcelDAOTest.java
@@ -0,0 +1,55 @@
+package app.DAO;
+
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParcelDAOTest {
+
+    private static EntityManagerFactory emf;
+    private static ParcelDAO parcelDAO;
+
+    @BeforeAll
+    static void setup() {
+        HibernateConfig.setTest(true);  // use Testcontainers
+        emf = HibernateConfig.getEntityManagerFactory();
+        parcelDAO = new ParcelDAO(emf);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+
+    @BeforeEach
+    void cleanDatabase() {
+        // To reset database if needed.
+    }
+
+    @Test
+    void createParcel() {
+    }
+
+    @Test
+    void retrieveParcelByTrackingNumber() {
+    }
+
+    @Test
+    void retrieveAllParcels() {
+    }
+
+    @Test
+    void updateDeliveryStatus() {
+    }
+
+    @Test
+    void removeParcel() {
+    }
+}
